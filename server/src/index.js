36289636a76f1788d70a4160782b5f5a07581bd6
darsal03 <darsalia2018@gmail.com>
2021-10-12 18:40:25 +0400

    add : connection to the MongoDb database

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,9 @@ import express from 'express'
 import { configCors } from './middlewares/cors.js'
 import { globalErrorHandler } from './middlewares/error.js'
 import { router } from './router.js'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 app.use(express.json())
@@ -13,6 +16,13 @@ app.use('/api', router)
 
 // eslint-disable-next-line no-unused-vars
 app.use(globalErrorHandler)
+
+mongoose
+  .connect(
+    `mongodb+srv://darso:${process.env.DB_PASS}@cluster0.p4ngx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => console.log('connected to DB'))
+  .catch((err) => console.log(err))
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
