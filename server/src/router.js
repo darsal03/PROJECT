a51ad03abba3991a4e796865bfda7dcd159774a4
darsal03 <darsalia2018@gmail.com>
2021-10-20 13:19:40 +0400

    add : Meals model

--- server/src/router.js
+++ server/src/router.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { getUserById, getUsers, createUser, login, logout } from './controllers/users.js'
-
+import { createMeal } from './controllers/meals.js'
 export const router = express.Router()
 
 /**
@@ -11,3 +11,10 @@ router.post('/users', createUser)
 router.get('/users/:id', getUserById)
 router.post('/login', login)
 router.post('/logout', logout)
+/*
+ Meals 
+*/
+// router.get('/meals',getMeals)
+router.post('/meals', createMeal)
+//router.get('meal/:id',getMealById)
+//router.delete('meals'deleteMeal)
