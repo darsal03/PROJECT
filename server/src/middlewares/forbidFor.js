d6ff1e0baef41287440f952d121090eb3928befb
darsal03 <darsalia2018@gmail.com>
2021-10-29 15:28:55 +0400

    add : forbidFor middleware

---
+++ server/src/middlewares/forbidFor.js
@@ -0,0 +1,8 @@
+export const forbidFor = (roles) => (req, res, next) => {
+  if (roles.includes(req.user.role)) {
+    return res.status(403).json({
+      msg: 'forbidden',
+    })
+  }
+  next()
+}
