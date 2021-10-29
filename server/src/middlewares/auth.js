537722f2549b5ccceebb91d176bf94475c719c86
darsal03 <darsalia2018@gmail.com>
2021-10-29 15:29:37 +0400

    add : auth middleware

---
+++ server/src/middlewares/auth.js
@@ -0,0 +1,11 @@
+import { Users } from '../models/User.js'
+export const auth = async (req, res, next) => {
+  if (!req.session.userId) {
+    return res.status(401).json({
+      msg: 'authentication failed',
+    })
+  } else {
+    req.user = await Users.findOne({ _id: req.session.userId })
+    next()
+  }
+}
