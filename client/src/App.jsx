04e8ff841630b9006ca9b3d9c1b12e04737379bc
darsal03 <darsalia2018@gmail.com>
2021-11-12 22:18:20 +0400

    refactor : code in App.js, add : react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,19 @@
 import { useUsers } from './hooks/use-users'
-import { Logo } from './components/svgs/logo'
+
+import { Login, Register } from './containers/Login'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const { data: users, error, isFetching } = useUsers()
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <Logo width="100" height="100" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-      <section>
-        <h2>Users</h2>
-        {isFetching ? (
-          <p>Loading...</p>
-        ) : error ? (
-          <p>{error}</p>
-        ) : (
-          <ul>
-            {users.map((user) => (
-              <li key={user.key}>{user.name}</li>
-            ))}
-          </ul>
-        )}
-      </section>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
