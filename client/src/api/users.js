0c3f3f7de234cabfdd2183e833c282063b627fcb
darsal03 <darsalia2018@gmail.com>
2021-11-24 20:28:19 +0400

    add : useRegistration hook in the hook dir, add : createUser func. in user.js

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,3 +1,13 @@
 const apiUrl = process.env.REACT_APP_API_BASE_URL
 
 export const getUsers = () => fetch(`${apiUrl}/users`)
+
+export const createUser = (form) => {
+  fetch(`${apiUrl}/users/` || 'http://localhost:8080/api/users/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(form),
+  })
+}
